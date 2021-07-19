6c7c68790e496a0d2932a840c5e5caf491fb6354
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-07-19 15:31:56 -0500

    notes

--- java-io-project/src/ContactsManagerApp.java
+++ java-io-project/src/ContactsManagerApp.java
@@ -9,3 +9,17 @@ public class ContactsManagerApp {
         // using hashmap, list, or set.
     }
 }
+
+
+// after path,
+// have a list
+// set it equal toa new array list
+// then for through it String lines: line
+    // if /else // if: if line.contains or .equals  it will get new
+// array list and use .remove(user input)
+// call new list .add() new line
+//
+
+//for (String file: contact) {
+//
+//        }
